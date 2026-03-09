134bdd2a66eb07195ec2955f9859f7a89a43ae8e
Dario Skidelsky <darioskidelsky@gmail.com>
2026-03-09 17:48:16 -0300

    add update index script

---
+++ update-mnt-index.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MNT_PATH="/mnt"
+DBS_DIR="/home/$USER/.local/share/ulauncher/extensions/com.github.darioque.everywhere/dbs"
+mkdir -p "$DBS_DIR"
+for drive in "$MNT_PATH"/*/; do
+  [ -d "$drive" ] || continue
+  safe=$(basename "$drive" | tr ":" "_" | tr " " "_")
+  updatedb -l 0 -o "$DBS_DIR/mnt_${safe}.db" -U "$drive"
+done
